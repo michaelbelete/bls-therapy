1576d749598a2044edd16a44d1c1c9830a2fd20f
Michael Belete <it.michael.belete@gmail.com>
2022-07-14 21:45:37 +0300

    finished animation speed

--- src/routes/therapist/session/[id].svelte
+++ src/routes/therapist/session/[id].svelte
@@ -4,64 +4,39 @@
 		isBouncing = !isBouncing;
 	};
 
-	let color = '#ef4444';
+	let bgColor = '#ef4444';
 	let speed = 2;
-    let animationSpeed = speed+'s';
+	$: animationSpeed = (10.5-speed) + 's';
 
-	function setCssVariables(node, variables) {
-		for (const name in variables) {
-			node.style.setProperty(`--${name}`, variables[name]);
-		}
-	}
-
-	function cssVariables(node, variables) {
-		setCssVariables(node, variables);
-
-		return {
-			update(variables) {
-				setCssVariables(node, variables);
-			}
-		};
-	}
-
-    const decreaseSpeed = () => {
-        speed-=0.5;
-        animationSpeed = (10.5-speed)+'s';
-    };
-
-    const increaseSpeed = () => {
-        speed+=0.5;
-        animationSpeed = (10.5-speed)+'s';
-    };
-
+	$: console.log(animationSpeed)
 </script>
 
 <div>
-	<main class="w-full h-52 flex items-center {isBouncing ? '' : 'justify-center'} bg-gray-1000 ">
+	<main class="w-full h-52 flex items-center {isBouncing ? '' : 'justify-center'} bg-gray-1000" style="--bg-color: {bgColor}; --animation-speed:{animationSpeed}">
 		<div
-			class="{isBouncing ? 'ball' : ''} w-20 h-20 rounded-full bg-red-500"
-            use:cssVariables={{color, animationSpeed}}
+			class="{isBouncing ? 'bounce' : ''} ball w-20 h-20 rounded-full"
 		/>
 	</main>
 	<p>Start/Stop</p>
-	<button type="submit" on:click={toggleBouncing} class="bg-red-300"
-		>{isBouncing ? 'Start' : 'Stop'}</button
+	<button type="submit" on:click={toggleBouncing} class="bg-green-300"
+		>{isBouncing ? 'Stop' : 'Start'}</button
 	>
 	<p>Change Color</p>
-	<input type="color" bind:value={color} />
+	<input type="color" bind:value={bgColor} />
 	<p>Set Speed</p>
-    <div class="flex flex-row gap-4">
-        <button type="submit" on:click={decreaseSpeed}  class="bg-red-700 px-4 py-2 disabled:bg-gray-700" disabled={speed == 0.5}>-</button>
-        <p class="text-lg py-2">{speed}</p>
-        <button type="submit" on:click={increaseSpeed}  class="bg-blue-700 px-4 py-2 disabled:bg-gray-700" disabled={speed == 10}>+</button>
-    </div>
-    <p>Share link: http://www.blm.io/session</p>
+	<div class="flex flex-row gap-4">
+		<input type="range" bind:value={speed} min="1" max="10" />
+	</div>
+	<p>Share link: http://www.blm.io/session</p>
 </div>
 
 <style>
 	.ball {
-		animation: bounce var(--animationSpeed);
-        background-color: var(--color);
+		animation: bounce;
+		animation-duration: var(--animation-speed);
+		background: radial-gradient(circle at 30% 30%, var(--bg-color), #774400);
+	}
+	.bounce {
 		animation-direction: alternate;
 		animation-timing-function: cubic-bezier(0.5, 0.5, 0.5, 0.5);
 		animation-iteration-count: infinite;
